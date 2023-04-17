bd7e2223537225259709cb20d26ed9cfe5ac94a1
Dan Pham <yungbloodx02@gmail.com>
2023-04-17 13:56:58 +0700

    file validator

--- src/features/collection/controller/collection.controller.ts
+++ src/features/collection/controller/collection.controller.ts
@@ -1,25 +1,47 @@
 import {
   Body,
   Controller,
+  HttpException,
   Post,
   UploadedFile,
   UseGuards,
   UseInterceptors,
-  UsePipes,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { environments } from 'src/environments/environments';
-import { CurrentUser } from 'src/features/auth/decorators/current-user.decorator';
 import { JwtAuthGuard } from 'src/features/auth/guard/jwt-auth.guard';
-import { User } from 'src/features/user/schema/user.schema';
 import { SharpPipe } from 'src/shared/pipe/sharp.pipe';
+import { UploadFileInput } from '../dto/upload-file.input';
+
+const uploadOptions = {
+  fileFilter(req, file, callback) {
+    if (!file.size || file.size > 1024 * 5) {
+      return callback(new HttpException('File too large', 400), false);
+    }
+
+    if (!file.originalname.match(/\.(png|jpg|jpeg|webp)$/)) {
+      return callback(
+        // throw error to json response
+        new HttpException(
+          'Invalid file type, only allowed file type .png .jpg .jpeg .webp',
+          400,
+        ),
+        false,
+      );
+    }
+    return callback(null, true);
+  },
+};
 
 @Controller('collection')
 export class CollectionController {
   @Post('upload-image')
   @UseGuards(JwtAuthGuard)
-  @UseInterceptors(FileInterceptor('image'))
-  async uploadImage(@UploadedFile(SharpPipe) image: Express.Multer.File) {
+  @UseInterceptors(FileInterceptor('image', uploadOptions))
+  async uploadImage(
+    @Body() fileInput: UploadFileInput,
+    @UploadedFile(SharpPipe) image: Express.Multer.File,
+  ) {
     return {
       url: `${environments.backendUrl}/public/images/${image}`,
     };
---
+++ src/shared/constants/mime-types.ts
@@ -0,0 +1,6 @@
+export const imageMimeType = [
+  'image/png',
+  'image/jpeg',
+  'image/jpg',
+  'image/webp',
+];
